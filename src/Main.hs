95c0e4cc5803a38262354fd507ea9640811a8df3
torquescrew <sugto555@gmail.com>
2013-08-11 12:04:26 +1200

    more code

--- src/Main.hs
+++ src/Main.hs
@@ -1,26 +1,16 @@
 {-# LANGUAGE DoAndIfThenElse #-}
 
---import qualified Data.ByteString as B
---import Data.Text (Text)
---import Data.Text.Encoding (encodeUtf8)
---import qualified Data.Text as T
---import qualified Data.Text.IO as I
---import qualified Data.ByteString as B
-
 import System.Directory (doesFileExist, getDirectoryContents)
 import System.FilePath ((</>))
 import Data.String.Utils
 import qualified Data.ByteString.Char8 as L
---import qualified Data.ByteString.Lazy.Char8 as L
 
 process :: FilePath -> IO ()
 process filename | isCpp filename  =  do 
 					contents <- L.readFile filename
-					--L.putStr (L.unlines (L.lines contents))
-					--return()
-					--putStr_Utf16LE contents
 					putStr (getIncludes (lines (L.unpack contents)))
                  | otherwise = return()
+
 
 mapDir :: (FilePath -> IO ()) -> FilePath -> IO ()
 mapDir proc fp = do
@@ -29,9 +19,6 @@ mapDir proc fp = do
   else getDirectoryContents fp >>=
        mapM_ (mapDir proc . (fp </>)) . filter (`notElem` [".", ".."])
 
-
---getIncludes' :: [Text] -> Text
---getIncludes' l = unlines (filter )
 
 
 getIncludes :: [String] -> String
@@ -45,18 +32,12 @@ isCpp fileName = endswith ".h" fileName || endswith ".cpp" fileName
 isIncludeStatement :: String -> Bool
 isIncludeStatement = startswith "#include" . lstrip
 
+getIncludeName :: String -> String
+getIncludeName s = takeWhile (\x -> x /= '"') s
 
---isIncludeStatement' :: Text -> Bool
---isIncludeStatement' = startswith "#include" . lstrip
-
---myPutStr :: String -> IO ()
---myPutStr t = putStr_Utf16LE (T.pack t)
-
---putStr_Utf16LE :: T.Text -> IO ()
---putStr_Utf16LE t = B.putStr (encodeUtf8 t)	
 
 main :: IO ()
-main = mapDir process "/Users/tobysuggate/Desktop/LCWM4/"
---main = mapDir process "/Users/tobysuggate/Desktop/LCWM4/CrossPlatform/Portable/CrossPlatform/ADIDat/DatCore/"
+--main = mapDir process "/Users/tobysuggate/Desktop/LCWM4/"
+main = mapDir process "/Users/tobysuggate/Desktop/LCWM4/CrossPlatform/Portable/CrossPlatform/ADIDat/DatCore/"
 --main = mapDir process "/Users/tobysuggate/Desktop/untitledfolder/"
 --main = mapDir process "/Users/tobysuggate/Desktop/LCWM4/LabChart/PCDevelop/LabChart/Calculations/"
