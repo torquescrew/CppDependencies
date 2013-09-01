3e39a5735aad6938be36450a14b5db9e3770f85e
torquescrew <sugto555@gmail.com>
2013-09-01 13:45:47 +1200

    add extract string function

--- workspace/Dependancies/.dist-buildwrapper/src/Blocks.hs
+++ workspace/Dependancies/.dist-buildwrapper/src/Blocks.hs
@@ -1,6 +1,6 @@
 module Blocks where
 
-import RemoveComments
+import           RemoveComments
 
 myFile :: [Char]
 myFile = "/Users/tobysuggate/Desktop/LCWM4/LabChartEssentials/LabChart/ChartDraw/ChartDrawer.h"
@@ -12,4 +12,14 @@ main = do
 
 --parseStatement :: String -> [String]
 
+a = "\"wib\");"
 
+
+extrStr :: String -> (String, String)
+extrStr code = extrStr' "" code
+
+extrStr' :: String -> String -> (String, String)
+extrStr' ""  ('"':code) = extrStr' "\"" code
+extrStr' str ('"':code) = (reverse('"':str), code)
+extrStr' str (c  :code) = extrStr' (c:str) code
+extrStr' str  code      = (str, code)
