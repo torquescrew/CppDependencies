81c1e7965611af2213ddd60b6defee86c91c313c
torquescrew <sugto555@gmail.com>
2013-09-21 21:16:35 +1200

    more code

--- workspace/Dependancies/.dist-buildwrapper/src/Blocks.hs
+++ workspace/Dependancies/.dist-buildwrapper/src/Blocks.hs
@@ -1,18 +1,22 @@
-module Blocks where
+module Main where
 
-import           RemoveComments
+import RemoveComments
 
 myFile :: [Char]
 myFile = "/Users/tobysuggate/Desktop/LCWM4/LabChartEssentials/LabChart/ChartDraw/ChartDrawer.h"
+
+--myFile = "/Users/tobysuggate/Desktop/LCWM4/Libs/CEF/include/internal/cef_types_wrappers.h"
 
 main :: IO ()
 main = do
        inpStr <- readFile myFile
        putStr (removeComments inpStr)
 
---parseStatement :: String -> [String]
 
-a = "\"wib\");"
+--a = "\"wib\");"
+typeDecKeywords = ["class", "struct", "typedef", "enum", "enum class"]
+operators = [':',';','!','<','>','{','}','*','(',')','&',',','-','+','=',' ','.','?']
+doubleOps = ["--","++","->","==","<=",">=","::","&&","||","**","/=","!=","+=","-=","*=","()"]
 
 
 extrStr :: String -> (String, String)
@@ -23,3 +27,9 @@ extrStr' ""  ('"':code) = extrStr' "\"" code
 extrStr' str ('"':code) = (reverse('"':str), code)
 extrStr' str (c  :code) = extrStr' (c:str) code
 extrStr' str  code      = (str, code)
+
+
+toStatement :: String -> String -> [String]
+toStatement part c@('"':code) = (fst res):toStatement (snd res)
+                                where res = extrStr c
+toStatement code 
--- workspace/Dependancies/src/Blocks.hs
+++ workspace/Dependancies/src/Blocks.hs
@@ -1,18 +1,24 @@
-module Blocks where
+module Main where
 
-import           RemoveComments
+import RemoveComments
+import Data.List.Utils
 
 myFile :: [Char]
 myFile = "/Users/tobysuggate/Desktop/LCWM4/LabChartEssentials/LabChart/ChartDraw/ChartDrawer.h"
+
+--myFile = "/Users/tobysuggate/Desktop/LCWM4/Libs/CEF/include/internal/cef_types_wrappers.h"
 
 main :: IO ()
 main = do
        inpStr <- readFile myFile
        putStr (removeComments inpStr)
 
---parseStatement :: String -> [String]
 
-a = "\"wib\");"
+--a = "\"wib\");"
+typeDecKeywords = ["class", "struct", "typedef", "enum", "enum class"]
+statementBreaks = ";{}"
+operators = [':',';','!','<','>','{','}','*','(',')','&',',','-','+','=',' ','.','?']
+doubleOps = ["--","++","->","==","<=",">=","::","&&","||","**","/=","!=","+=","-=","*=","()"]
 
 
 extrStr :: String -> (String, String)
@@ -23,3 +29,30 @@ extrStr' ""  ('"':code) = extrStr' "\"" code
 extrStr' str ('"':code) = (reverse('"':str), code)
 extrStr' str (c  :code) = extrStr' (c:str) code
 extrStr' str  code      = (str, code)
+
+
+beginsWithOp :: String -> Bool
+beginsWithOp code = hasAny [take 2 code] doubleOps
+
+--TODO
+toTokens :: String -> String -> [String]
+toTokens statement c@('"':code) = (fst res):toTokens statement (snd res)
+                                      where res = extrStr c
+toTokens statement code | beginsWithOp code = [code]
+
+
+hasBreak :: String -> Bool
+hasBreak code@(c:_) = hasAny [c] statementBreaks || endOfInclude code
+
+
+endOfInclude :: String -> Bool
+endOfInclude statement = startswith "#include " statement && (length statement) > 11
+
+
+toStatements :: String -> String -> [String]
+toStatements statement code 
+             | hasBreak code      = newStatement:(toStatements [] (tail code))
+                                    where newStatement = reverse $ (head code):statement
+toStatements statement ('#':code) = statement:(toStatements "#" code)
+toStatements statement (c:code)   = toStatements (c:statement) code
+toStatements statement []         = [[]]
