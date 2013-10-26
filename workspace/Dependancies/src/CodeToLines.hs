89913fe3c92786a5ed2517a3e47046ec355bbca2
torquescrew <sugto555@gmail.com>
2013-10-26 15:13:57 +1300

    tidy up

--- workspace/Dependancies/src/CodeToLines.hs
+++ workspace/Dependancies/src/CodeToLines.hs
@@ -10,28 +10,11 @@ lineBreaks :: [Char]
 lineBreaks = ";{}"
 
 
--- visBreak :: [String]
--- visBreak = ["public","private","protected"]
-
 
 toLines :: String -> [String]
 toLines = ts2 ""
 
 
--- ts :: String -> String -> [String]
--- -- ts line (
--- ts line code 
---     | hasBreak code       = ns:(ts [] (tail code))
---     | startswith "#" code = (fst lines'):(ts "" (snd lines'))
---     -- TODO isVisMod code
---     -- | isVisMod' code      
---     | null code           = [] -- does this throw away line?
---     | otherwise           = ts ((head code):line) (tail code)
---        where ns     = reverse $ (head code):line
---              lines' = splitAtNL code
-
-
---TODO: need to handle template < >, should result in new line
 ts2 :: String -> String -> [String]
 ts2 l  c | hasBreak c              = nl:(ts2 [] (tail c))
                                      where nl = reverse (head c:l)
@@ -60,10 +43,6 @@ hasBreak (c:_) = hasAny [c] lineBreaks
 hasBreak  _    = False
 
 
--- hasTemplate :: String -> Bool
--- hasTemplate 
-
-
 isNewLine :: String -> Bool
 isNewLine code = startswith "\n" code || startswith "\r" code || startswith "\r\n" code
 
@@ -74,34 +53,10 @@ breakAfter p l = splitAt i l
                  where i = fromJust (findIndex p l) + 1
 
 
-
--- splitAtNL :: String -> (String, String)
--- splitAtNL code = spltNL "" code
-
-
--- spltNL :: String -> String -> (String, String)
--- spltNL line code | isNewLine code = ((reverse line), code)
---                  | null code      = ((reverse line), "")
---                  | otherwise      = spltNL ((head code):line) (tail code)
-
-
--- breakM :: ([a] -> Bool) -> [a] -> [a] -> ([a], [a])
--- breakM p l c | p c = ((reverse l), c)
---              | null c = ((reverse l), [])
---              | otherwise = breakM p ((head c):l) (tail c)
-
-
--- isVisMod :: String -> Bool
--- isVisMod c | startswith "public" c = startswith ":" (dropWhile isSpace (drop 6 c))
--- isVisMod c | startswith "private" c = startswith ":" (dropWhile isSpace (drop 7 c))
--- isVisMod c | startswith "protected" c = startswith ":" (dropWhile isSpace (drop 9 c))
--- isVisMod _ = False
-
 isVisMod' :: String -> Maybe Int
-isVisMod' c | startswith "public" c && startswith ":" (dropWhile isSpace (drop 6 c)) = (elemIndex ':' c)
+isVisMod' c | startswith "public" c && startswith ":" (dropWhile isSpace (drop 6 c)) = elemIndex ':' c
 isVisMod' c | startswith "private" c && startswith ":" (dropWhile isSpace (drop 7 c)) = elemIndex ':' c
 isVisMod' c | startswith "protected" c && startswith ":" (dropWhile isSpace (drop 9 c)) = elemIndex ':' c
 isVisMod' _ = Nothing
 
--- splitVis :: String -> [String]
--- splitVis code 
+
